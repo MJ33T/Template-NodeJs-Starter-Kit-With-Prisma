f1199860b019981f28b632a95e44f5fc21c34ee6
Md. Muhtasim Jeet <57042167+MJ33T@users.noreply.github.com>
2026-02-01 11:23:01 +0600

    initialization : Prisma 6.0.0 initialize and test done

---
+++ src/controllers/TestController.js
@@ -0,0 +1,40 @@
+import prisma from "../config/prisma.js";
+
+export const testController = async (req, res) => {
+  try {
+    // simple query – replace `user` with any existing model
+    const result = await prisma.user.findMany({ take: 1 });
+
+    res.json({
+      message: "Prisma is working 🚀",
+      db: result,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Prisma test failed ❌",
+      error: error.message,
+    });
+  }
+
+//   try {
+//     const user = await prisma.user.create({
+//       data: {
+//         name: "Test User",
+//         email: `test_${Date.now()}@example.com`, // avoid unique conflict
+//       },
+//     });
+
+//     res.json({
+//       message: "User created successfully 🚀",
+//       user,
+//     });
+//   } catch (error) {
+//     console.error(error);
+
+//     res.status(500).json({
+//       message: "Prisma create failed ❌",
+//       error: error.message,
+//     });
+//   }
+};
