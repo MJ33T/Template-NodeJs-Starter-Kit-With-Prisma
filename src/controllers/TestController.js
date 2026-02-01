c39f37550127c7af3dc851c5b1a0a38b86704f8e
Md. Muhtasim Jeet <57042167+MJ33T@users.noreply.github.com>
2026-02-01 11:54:07 +0600

    fix : update prisma stable version

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,40 +1,36 @@
+import { errorLogger } from "../config/logger.js";
 import prisma from "../config/prisma.js";
-
 export const testController = async (req, res) => {
+  // try {
+  //   // simple query – replace `user` with any existing model
+  //   const result = await prisma.user.findMany();
+  //   res.json({
+  //     message: "Prisma is working 🚀",
+  //     db: result,
+  //   });
+  // } catch (error) {
+  //   errorLogger.error("Prisma test failed", { error });
+  //   res.status(500).json({
+  //     message: "Prisma test failed ❌",
+  //     error: error.message,
+  //   });
+  // }
   try {
-    // simple query – replace `user` with any existing model
-    const result = await prisma.user.findMany({ take: 1 });
-
+    const user = await prisma.user.create({
+      data: {
+        name: "Test User",
+        email: `test_${Date.now()}@example.com`, // avoid unique conflict
+      },
+    });
     res.json({
-      message: "Prisma is working 🚀",
-      db: result,
+      message: "User created successfully 🚀",
+      user,
     });
   } catch (error) {
-    console.error(error);
+    errorLogger.error(error.message);
     res.status(500).json({
-      message: "Prisma test failed ❌",
+      message: "Prisma create failed ❌",
       error: error.message,
     });
   }
-
-//   try {
-//     const user = await prisma.user.create({
-//       data: {
-//         name: "Test User",
-//         email: `test_${Date.now()}@example.com`, // avoid unique conflict
-//       },
-//     });
-
-//     res.json({
-//       message: "User created successfully 🚀",
-//       user,
-//     });
-//   } catch (error) {
-//     console.error(error);
-
-//     res.status(500).json({
-//       message: "Prisma create failed ❌",
-//       error: error.message,
-//     });
-//   }
 };
