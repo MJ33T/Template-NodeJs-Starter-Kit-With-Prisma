a56310d77fb882663adc0cf135fc3ddf8ceba671
Md. Muhtasim Jeet <57042167+MJ33T@users.noreply.github.com>
2026-02-02 17:10:27 +0600

    fix : structure fixed

--- src/middleware/formDataParser.js
+++ src/middleware/formDataParser.js
@@ -1,32 +1,5 @@
 import responseHelper from "../utils/responseHelper.js";
 
-// export const formDataParser = (req, res, next) => {
-//   try {
-//     const method = req.method.toUpperCase();
-//     // Only parse for POST, PUT, PATCH requests
-//     if (!["POST", "PUT", "PATCH"].includes(method)) {
-//       return next();
-//     }
-
-//     if (!req.body || !req.body.data) {
-//       return responseHelper.failed(res, "Invalid JSON in 'data' field", 422);
-//     }
-
-//     if (typeof req.body.data !== "string") {
-//       return responseHelper.failed(res, "Invalid JSON in 'data' field", 422);
-//     }
-
-//     // Parse JSON safely
-//     const parsedData = JSON.parse(req.body.data);
-
-//     req.body = { ...req.body, ...parsedData };
-//     delete req.body.data; // Remove the original data field
-//     next();
-//   } catch (error) {
-//     return responseHelper.failed(res, "Invalid JSON in 'data' field", 422);
-//   }
-// };
-
 export const formDataParser = (req, res, next) => {
   try {
     if (req.body && req.body.data) {
